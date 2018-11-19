ff55025b01af5b89a5995c95d6f33a9c7e5559d5
Joao Gularte <joao.gularte@logycware.com>
2018-11-19 13:29:49 -0200

    :)

--- event-emitter/event-emitter-inherits.js
+++ event-emitter/event-emitter-inherits.js
@@ -1,24 +1,57 @@
-"use strict";
+//"use strict"
 
-const util = require('util');
-const eventEmitter = require('events').EventEmitter;
-const fs = require('fs');
+var util = require('util');
+var eventEmitter = require('events').EventEmitter;
+var fs = require('fs');
+
+
 
 function InputChecker(name, file){
     this.name = name;
     this.writeStream = fs.createWriteStream('./'+file+'.txt', {
         flags:'a', //O arquivo é criado se não existir
-        enconding: 'utf8',
-        mode: 0o666
+        enconding: 'utf8'
     });
 }
+
 
 util.inherits(InputChecker, eventEmitter); //realiza a extão de eventEmitter para InputChecker
 
 InputChecker.prototype.check = function(input ){
     var comando = input.trim().substr(0,3);
-    console.log(comando);
+
+
+    if (comando === "wr:"){
+        this.emit('write', input.trim().substr(3, input.length - 1));
+    }else if(comando === "en:"){
+        this.emit('end');
+    }else{
+        this.emit('echo', input);
+    }
 }
 
-const check = new InputChecker('Joao', 'joia');
-check.check("wr:tudo bem ");
+inputCheck = new InputChecker("João", "saida");
+
+inputCheck.on('write', function(data){
+    this.writeStream.write(data, 'utf8');
+});
+
+inputCheck.on('echo', function(data){
+    process.stdout.write(inputCheck.name + ' escreveu ' + data);
+});
+
+inputCheck.on('end', function(){
+    process.exit();
+})
+
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', function(){
+    var input = process.stdin.read();
+    if(input !== null){
+        inputCheck.check(input);
+    }
+})
+
+
+
